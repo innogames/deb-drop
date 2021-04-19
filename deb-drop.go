f16f8a472ec102e9db3609787d354de71d876e24
Patrick Schmidt <patrick.schmidt@innogames.com>
2021-04-19 11:21:49 +0200

    Migrate to aptly

--- deb-drop.go
+++ deb-drop.go
@@ -15,6 +15,9 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"sync"
+	"sync/atomic"
+	"time"
 
 	"github.com/BurntSushi/toml"
 	"github.com/mcuadros/go-version"
@@ -71,22 +74,118 @@ func main() {
 		lg.Println("Error:", err)
 	}
 
-	http.HandleFunc("/", makeHandler(lg, &config, mainHandler))
+	h := newHandler(&config, lg)
+	http.HandleFunc("/", h.mainHandler)
 	err = fcgi.Serve(l, nil)
+	// err = http.Serve(l, nil)
 
 	if err != nil {
 		lg.Println("Error:", err)
 	}
 }
 
-func makeHandler(lg *log.Logger, config *Config, fn func(http.ResponseWriter, *http.Request, *Config, *log.Logger)) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		fn(w, r, config, lg)
+type handler struct {
+	config      *Config
+	lg          *log.Logger
+	mu          *sync.Mutex
+	t           *time.Ticker            // batch timer
+	q           map[string][]chan error // distributions to publish
+	adds        int32                   // how many adds still in progress
+	lastPublish time.Time
+}
+
+func newHandler(config *Config, lg *log.Logger) *handler {
+	h := &handler{
+		config:      config,
+		lg:          lg,
+		mu:          new(sync.Mutex),
+		t:           time.NewTicker(1000 * time.Millisecond),
+		q:           make(map[string][]chan error),
+		adds:        0,
+		lastPublish: time.Now(),
+	}
+
+	go h.publishWorker()
+
+	return h
+}
+
+func (h *handler) startAdd() {
+	atomic.AddInt32(&h.adds, 1)
+}
+
+func (h *handler) finishAdd() {
+	atomic.AddInt32(&h.adds, -1)
+}
+
+func (h *handler) waitPublished(dist string) error {
+	h.mu.Lock()
+	h.t.Reset(1000 * time.Millisecond)
+
+	if _, ok := h.q[dist]; !ok {
+		h.q[dist] = make([]chan error, 0, 1)
+	}
+
+	c := make(chan error, 1)
+	defer close(c)
+
+	h.q[dist] = append(h.q[dist], c)
+	h.mu.Unlock()
+	h.lg.Printf("wait for publish of %s..\n", dist)
+
+	return <-c
+}
+
+func (h *handler) publishBatch() {
+	// check for new publish requests
+	var n int
+	var d string
+	for dist, waiters := range h.q {
+		if len(waiters) > n {
+			n = len(waiters)
+			d = dist
+		}
+	}
+
+	if n == 0 {
+		return
+	}
+
+	// publish distribution
+	err := h.publishDist(d)
+
+	// notify waiters that it is done
+	for _, waiter := range h.q[d] {
+		waiter <- err
+	}
+
+	delete(h.q, d)
+}
+
+func (h *handler) publishWorker() {
+	for {
+		h.mu.Lock()
+		<-h.t.C
+		adds := atomic.LoadInt32(&h.adds)
+
+		// skip if there are still changes in progress, but force a batch after some time..
+		if adds > 0 && time.Now().Sub(h.lastPublish) < 15*time.Second {
+			h.lastPublish = time.Now()
+			h.mu.Unlock()
+			continue
+		}
+
+		if adds > 0 {
+			h.lg.Printf("still %d uploads in progress but no publish in %s. forcing\n", adds, time.Now().Sub(h.lastPublish).String())
+		}
+
+		h.publishBatch()
+		h.lastPublish = time.Now()
+		h.mu.Unlock()
 	}
 }
 
-func mainHandler(w http.ResponseWriter, r *http.Request, config *Config, lg *log.Logger) {
-
+func (h *handler) mainHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "HEAD" {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintln(w, "Hello healthcheck")
@@ -94,23 +193,21 @@ func mainHandler(w http.ResponseWriter, r *http.Request, config *Config, lg *log
 	}
 
 	repos := strings.Split(r.FormValue("repos"), ",")
-	err := validateRepos(lg, config.RepoLocation, repos)
+	err := validateRepos(h.lg, h.config.RepoLocation, repos)
 	if err != nil {
 		w.WriteHeader(http.StatusNotFound)
-		lg.Println(err)
 		fmt.Fprintln(w, err)
 		return
 	}
 
-	err = validateToken(lg, config, r.FormValue("token"), repos)
+	err = validateToken(h.lg, h.config, r.FormValue("token"), repos)
 	if err != nil {
 		w.WriteHeader(http.StatusForbidden)
-		lg.Println(err)
 		fmt.Fprintln(w, err)
 		return
 	}
 
-	//Check if old packages should be removed
+	// Check if old packages should be removed
 	keepVersions, err := strconv.Atoi(r.FormValue("versions"))
 	if err != nil || keepVersions < 1 {
 		keepVersions = 5
@@ -128,7 +225,7 @@ func mainHandler(w http.ResponseWriter, r *http.Request, config *Config, lg *log
 		content, header, err = r.FormFile("package")
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
-			lg.Println(err)
+			h.lg.Println(err)
 			fmt.Fprintln(w, err)
 			return
 		}
@@ -138,44 +235,21 @@ func mainHandler(w http.ResponseWriter, r *http.Request, config *Config, lg *log
 
 	if r.Method == "GET" {
 		// Package name needs to be validated only when we are making changes
-		err = validatePackageName(lg, packageName, false)
+		err := validatePackageName(h.lg, packageName, false)
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
-			lg.Println(err)
 			fmt.Fprintln(w, err)
 			return
 		}
 
-		if len(repos) != 1 {
-			w.WriteHeader(http.StatusBadRequest)
-			lg.Println("You should pass exactly 1 repo")
-			fmt.Fprintln(w, "You should pass exactly 1 repo")
-			return
-		}
-
-		pattern := config.RepoLocation + "/" + repos[0] + "/" + packageName + "*"
-		matches := getPackagesByPattern(pattern)
-		if len(matches) == 0 {
-			w.WriteHeader(http.StatusNotFound)
-			lg.Println(pattern + " is not found")
-			return
-		} else {
-			w.WriteHeader(http.StatusOK)
-			for i := 0; i < keepVersions; i++ {
-				element := len(matches) - 1 - i
-				if element < 0 {
-					break
-				}
-				fmt.Fprintln(w, path.Base(matches[element]))
-			}
-			return
-		}
+		h.list(w, repos, packageName, keepVersions)
+		return
 	} else if r.Method == "POST" {
 		// Allow caching of up to <amount> in memory before buffering to disk. In MB
-		err = r.ParseMultipartForm(int64(config.RequestCacheSize * 1024))
+		err = r.ParseMultipartForm(int64(h.config.RequestCacheSize * 1024))
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
-			lg.Println(err)
+			h.lg.Println(err)
 			fmt.Fprintln(w, err)
 			return
 		}
@@ -184,75 +258,105 @@ func mainHandler(w http.ResponseWriter, r *http.Request, config *Config, lg *log
 		defer r.MultipartForm.RemoveAll()
 
 		// Package name needs to be validated only when we are making changes
-		err = validatePackageName(lg, packageName, true)
+		err = validatePackageName(h.lg, packageName, true)
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
-			lg.Println(err)
+			h.lg.Println(err)
 			fmt.Fprintln(w, err)
 			return
 		}
 
 		repositories := repos
 
+		// This is used when package is passed as name, which means it is copy action
+		// Because for UPLOAD it is multipart.FileHeader
 		if r.FormValue("package") != "" {
-			/*
-				This is used when package is passed as name, which means it is copy action
-				Because for UPLOAD it is multipart.FileHeader
-			*/
-
-			// Open original file
-			content, err = os.Open(config.RepoLocation + "/" + repos[0] + "/" + packageName)
-			if err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-				lg.Println(err)
-				fmt.Fprintf(w, "Can not find original package %s in %s", packageName, repos[0])
-				return
-			}
-			defer content.Close()
-
 			// We need at least 2 repos to copy package between
 			if len(repos) < 2 {
 				w.WriteHeader(http.StatusBadRequest)
-				lg.Println("You should pass at least 2 repo")
+				h.lg.Println("You should pass at least 2 repo")
 				fmt.Fprintln(w, "You should pass at least 2 repo")
 				return
 			}
+
 			repositories = repos[1:]
+			err = h.copyPackage(repos[0], repositories, packageName, h.config.RepoLocation)
+			if err != nil {
+				if err.Error() == "package version already exists" {
+					w.WriteHeader(http.StatusConflict)
+				} else {
+					w.WriteHeader(http.StatusInternalServerError)
+				}
+				h.lg.Println(err)
+				fmt.Fprintln(w, err)
+				return
+			}
+			h.lg.Printf("done copying %s from %v\n", packageName, repos[0])
+		} else {
+			err = h.addToRepos(h.lg, h.config, content, repositories, packageName)
+			if err != nil {
+				if err.Error() == "package version already exists" {
+					w.WriteHeader(http.StatusConflict)
+				} else {
+					w.WriteHeader(http.StatusInternalServerError)
+				}
+				h.lg.Println(err)
+				fmt.Fprintln(w, err)
+				return
+			}
+			h.lg.Printf("done adding %s to %v\n", packageName, repositories)
 		}
 
-		err = addToRepos(lg, config, content, repositories, packageName)
-		if err != nil && r.FormValue("package") != "" {
-			// If COPY was performed and at the destination repo we already have this package - return not modified
-			w.WriteHeader(http.StatusNotModified)
-			lg.Println(err)
-			fmt.Fprintln(w, err)
-			return
-		} else if err != nil {
-			// If this was UPLOAD
-			w.WriteHeader(http.StatusConflict)
-			lg.Println(err)
-			fmt.Fprintln(w, err)
-			return
-		}
-
-		err = removeOldPackages(lg, config, repos, packageName, keepVersions)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			fmt.Fprintln(w, err)
-			return
-		}
+		// err = removeOldPackages(lg, config, repos, packageName, keepVersions)
+		// if err != nil {
+		// 	w.WriteHeader(http.StatusInternalServerError)
+		// 	fmt.Fprintln(w, err)
+		// 	return
+		// }
 	} else {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		fmt.Fprintln(w, "Unsupported method "+r.Method)
 		return
 	}
 
-	err = generateRepos(lg, config, repos)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+	// err = generateRepos(h.lg, h.config, repos)
+	// if err != nil {
+	// 	w.WriteHeader(http.StatusInternalServerError)
+	// 	fmt.Fprintln(w, err)
+	// 	return
+	// }
+}
+
+func (h *handler) list(w http.ResponseWriter, repos []string, packageName string, n int) error {
+	if len(repos) != 1 {
+		w.WriteHeader(http.StatusBadRequest)
+		err := fmt.Errorf("you should pass exactly 1 repo")
+		h.lg.Println(err)
 		fmt.Fprintln(w, err)
-		return
+
+		return err
 	}
+
+	matches := h.listPackages(repos[0], h.config.RepoLocation, packageName)
+	if len(matches) == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		err := fmt.Errorf("package %s not found", packageName)
+		h.lg.Println(err)
+		fmt.Fprintln(w, err)
+
+		return err
+	}
+
+	w.WriteHeader(http.StatusOK)
+	for i := 0; i < n; i++ {
+		element := len(matches) - 1 - i
+		if element < 0 {
+			break
+		}
+		fmt.Fprintln(w, path.Base(matches[element]))
+	}
+
+	return nil
 }
 
 func validateToken(lg *log.Logger, config *Config, token string, repos []string) error {
@@ -305,7 +409,7 @@ func validateRepos(lg *log.Logger, repoLocation string, repos []string) error {
 			return fmt.Errorf("%s", "Repo has invalid format")
 		}
 
-		stat, err := os.Stat(repoLocation + "/" + repo)
+		stat, err := os.Stat(repoLocation + "/" + parts[0] + "/dists/" + parts[0] + "/" + parts[1])
 		if err != nil {
 			lg.Println("Repository does not exist", err)
 			return fmt.Errorf("%s", "Repository does not exist")
@@ -439,5 +543,108 @@ func generateRepos(lg *log.Logger, config *Config, repos []string) error {
 			return fmt.Errorf("Could not generate metadata for %s : %v", name, err)
 		}
 	}
+	return nil
+}
+
+func (h *handler) addToRepos(lg *log.Logger, config *Config, content io.Reader, repos []string, packageName string) error {
+	tmpFilePath := fmt.Sprintf("%s/%s", config.TmpDir, packageName)
+	err := writeStreamToTmpFile(lg, content, tmpFilePath)
+	if err != nil {
+		return err
+	}
+	defer os.Remove(tmpFilePath)
+
+	dists := make(map[string]struct{})
+	for _, repo := range repos {
+		matches := h.listPackages(repo, config.RepoLocation, packageName)
+		for _, match := range matches {
+			if path.Base(match) == packageName {
+				return fmt.Errorf("package version already exists")
+			}
+		}
+
+		h.startAdd()
+		h.mu.Lock()
+		h.lg.Printf("add '%s' to '%s'\n", strings.TrimRight(packageName, ".deb"), repo)
+		cmd := exec.Command("sudo", "-u", "aptly", "aptly", "repo", "add", repo, tmpFilePath)
+		buf, err := cmd.CombinedOutput()
+		h.mu.Unlock()
+		h.finishAdd()
+		if err != nil {
+			return fmt.Errorf("could not add package %s to %s: %v\n%s", tmpFilePath, repo, err, string(buf))
+		}
+		h.lg.Printf("done adding '%s' to '%s'\n", strings.TrimRight(packageName, ".deb"), repo)
+
+		dist := strings.Split(repo, "-")[0]
+		dists[dist] = struct{}{}
+	}
+
+	for dist := range dists {
+		err = h.waitPublished(dist)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (h *handler) publishDist(dist string) error {
+	h.lg.Printf("publish %s..\n", dist)
+
+	cmd := exec.Command("sudo", "-u", "aptly", "aptly", "publish", "update", "--skip-contents", "--batch", "--passphrase-file", "/etc/aptly/passphrase", "--force-overwrite", dist, dist)
+	buf, err := cmd.CombinedOutput()
+	if err != nil {
+		err = fmt.Errorf("could not publish %s: %v\n%s", dist, err, string(buf))
+	}
+
+	return err
+}
+
+func (h *handler) listPackages(repo, repoLocation, packageName string) []string {
+	parts := strings.Split(repo, "-")
+	pattern := repoLocation + "/" + parts[0] + "/" + parts[0] + "/pool/" + parts[1] + "/*/*/" + packageName + "*"
+
+	return getPackagesByPattern(pattern)
+}
+
+func (h *handler) copyPackage(srcRepo string, dstRepos []string, packageName, repoLocation string) error {
+	matches := h.listPackages(srcRepo, repoLocation, packageName)
+	if len(matches) == 0 {
+		return fmt.Errorf("could not find package in %s", srcRepo)
+	}
+
+	dists := make(map[string]struct{})
+	for _, dstRepo := range dstRepos {
+		matches := h.listPackages(dstRepo, repoLocation, packageName)
+		for _, match := range matches {
+			if path.Base(match) == packageName {
+				return fmt.Errorf("package version already exists")
+			}
+		}
+
+		h.startAdd()
+		h.mu.Lock()
+		h.lg.Printf("copy '%s' from '%s' to '%s'\n", strings.TrimRight(packageName, ".deb"), srcRepo, dstRepo)
+		cmd := exec.Command("sudo", "-u", "aptly", "aptly", "repo", "copy", srcRepo, dstRepo, strings.TrimRight(packageName, ".deb"))
+		buf, err := cmd.CombinedOutput()
+		h.mu.Unlock()
+		h.finishAdd()
+		if err != nil {
+			return fmt.Errorf("could not copy package: %w\n%s", err, string(buf))
+		}
+		h.lg.Printf("done copying '%s' from '%s' to '%s'\n", strings.TrimRight(packageName, ".deb"), srcRepo, dstRepo)
+
+		dist := strings.Split(dstRepo, "-")[0]
+		dists[dist] = struct{}{}
+	}
+
+	for dist := range dists {
+		err := h.waitPublished(dist)
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
